39f01836e86830078f2395938a5f2400f5e6e9ff
Stalyin <stalyin16ac@gmail.com>
2026-08-15 21:37:11 -0500

    feat: mostrar costo total con dos decimales

--- ecoenvio/src/main/java/com/ecoenvio/Main.java
+++ ecoenvio/src/main/java/com/ecoenvio/Main.java
@@ -1,7 +1,25 @@
 package com.ecoenvio;
 
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Dimensiones dimensiones = new Dimensiones(20, 20, 20);
+
+        Cliente cliente = new Cliente(
+                "Juan",
+                "PREMIUM");
+
+        Envio envio = new Envio(
+                "ENV-001",
+                4,
+                10,
+                "TRANSFERENCIA",
+                dimensiones,
+                cliente);
+
+        double costoTotal = envio.calcularCostoTotal();
+
+        System.out.printf("Costo total del envío: $%.2f%n", costoTotal);
     }
 }
